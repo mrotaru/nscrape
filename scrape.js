21838c20c4efd91761bc2eb7097da89161906b87
Mihai Rotaru <mihai.rotaru@gmx.com>
2014-08-02 12:29:18 +0100

    refactor - mk a Scraper class

--- scrape.js
+++ scrape.js
@@ -2,31 +2,47 @@ var request = require('request');
 
 var spider_path = ('./spiders');
 
-var spider_name =process.argv[2];
-var Spider = require(spider_path + '/' + spider_name + '.js');
-var spider = new Spider();
-
 //var proxy = "http://127.0.0.1:8888"; // will be used by Request
 
-// start url
-var start_url = typeof spider.start_url == 'undefined' ? 'http://www.' + spider.name : spider.start_url;
-console.log('scraping: ', start_url);   
+var spider_name =process.argv[2];
 
-if(spider.phantom) {
+function Scraper(spider_name) {
+    this.init(spider_name);
+}
+
+Scraper.prototype.init = function(spider_name) {
+    var Spider = require(spider_path + '/' + spider_name + '.js');
+    this.spider = new Spider();
+    this.start_url = typeof this.spider.start_url == 'undefined' ? 'http://www.' + this.spider.name : this.spider.start_url;
+}
+
+Scraper.prototype.scrape = function(url){
+    var url = typeof url == 'undefined' ? this.start_url : url;
+    console.log('scraping: ', url);   
+    if(this.spider.phantom) {
+        this._phantomScrape(url);
+    } else {
+        this._requestScrape(url);
+    }
+}
+
+Scraper.prototype._phantomScrape = function(url){
+
+    var self = this;
     var _phantom = require("phantom");
     _phantom.create(function(phantom){
         if(!phantom) {
             console.log('phantom create failed');
         } else {
             phantom.createPage(function(page){
-                page.open(start_url, function(status){
+                page.open(url, function(status){
                     if(status == 'success') {
                         page.evaluate(function(){
                                 return document;
                             }, function (result) {
                                 var html = result.all[0].innerHTML;
                                 phantom.exit();
-                                spider.parse(html);
+                                self.spider.parse(html);
                         })
                     } else {
                         console.log("error: page could not be opened");
@@ -35,18 +51,24 @@ if(spider.phantom) {
             });
         }
     });
-} else {
+}
+
+Scraper.prototype._requestScrape = function(url){
 
     var request_options = {};
-    request_options.uri = start_url;
+    request_options.uri = url;
     request_options.proxy = typeof proxy != 'undefined' ? proxy : null;
 
+    var self = this;
     request(
         request_options,
         function(err, resp, body) {
            if (!err && resp.statusCode == 200) {
-                spider.parse(body);
+                self.spider.parse(body);
             }
         }
     )
 }
+
+var scraper = new Scraper(spider_name);
+scraper.scrape();
