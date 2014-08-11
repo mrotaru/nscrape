a3e643fd3b97e22eecd7f77901a980100ca2f9a8
Mihai Rotaru <mihai.rotaru@gmx.com>
2014-08-11 23:31:07 +0100

    wip promisification

--- scrape.js
+++ scrape.js
@@ -1,24 +1,47 @@
 var request = require('request');
+var fs = require('fs');
 
 var spider_path = ('./spiders');
 
 //var proxy = "http://127.0.0.1:8888"; // will be used by Request
 
 var spider_name =process.argv[2];
+var extractLinks = 2; // by default, extract 2 more links to be parsed
 
 function Scraper(spider_name) {
     this.init(spider_name);
+    this._scrapedLinks = 0;
 }
 
 Scraper.prototype.init = function(spider_name) {
-    var Spider = require('./node_modules/' + spider_name + '-spider');
+    var Spider = null;
+    // try to load spider from local dir
+    var localPath = spider_path + '/' + spider_name;
+    if(fs.existsSync(localPath)) {
+        Spider = require(localPath);
+    } else {
+        Spider = require('./node_modules/' + spider_name + '-spider');
+    }
     this.spider = new Spider();
     this.start_url = typeof this.spider.start_url == 'undefined' ? 'http://www.' + this.spider.name : this.spider.start_url;
 }
 
 Scraper.prototype.scrape = function(url){
     var url = typeof url == 'undefined' ? this.start_url : url;
-    console.log('scraping: ', url);   
+    var done = false;
+    while(!done) {
+        console.log('scraping: ', url);   
+        this._scrapedLinks++;
+        if(typeof this.spider.more == 'function' && this._scrapedLinks < extractLinks ) {
+            scrape(this.spider.more());
+        } else {
+            done = true;
+        }
+    }
+}
+
+// returns a promise
+Scraper.prototype._scrape = function(url){
     if(this.spider.phantom) {
         this._phantomScrape(url);
     } else {
@@ -26,8 +49,8 @@ Scraper.prototype.scrape = function(url){
     }
 }
 
+// promisify
 Scraper.prototype._phantomScrape = function(url){
-
     var self = this;
     var _phantom = require("phantom");
     _phantom.create(function(phantom){
@@ -53,6 +76,7 @@ Scraper.prototype._phantomScrape = function(url){
     });
 }
 
+// promisify
 Scraper.prototype._requestScrape = function(url){
 
     var request_options = {};
