cc45227f300590ca6652c1166ead7b96fba49b1e
Mihai Rotaru <mihai.rotaru@gmx.com>
2014-09-16 22:44:12 +0100

    add spider base class

---
+++ spider.js
@@ -0,0 +1,51 @@
+var cheerio      = require('cheerio');
+var util         = require("util");
+var EventEmitter = require("events").EventEmitter;
+var winston      = require('winston');
+var ee           = new EventEmitter();
+
+var log = winston.loggers.get('spider');
+
+function Spider(){
+    EventEmitter.call(this);
+    this.name = '';
+    this.baseUrl = '';
+
+    // set inside `parse`
+    this.items = [];
+    this._html = null;
+    this.$ = null;
+    this.currentPage = null;
+};
+
+util.inherits(Spider, EventEmitter);
+
+Spider.prototype.parse = function(html) {
+    log.info('parsing %d bytes', html.length);
+    var self = this;
+    self._html = html;
+    self.$ = cheerio.load(html);
+    self.$('.redesign-item-listing h2').each(function(i,el){
+        var item = {};
+        item.title = self.$(this).text();
+        self.items.push(item);
+        self.emit("item-scraped",item);
+    })
+}
+
+// Get the url to the next page
+Spider.prototype.nextUrl = function() {
+    var self = this;
+    if(self.currentPage === null) {
+        self.currentPage = 1;
+        log.info('info','setting currentPate to 1');
+        return self.baseUrl;
+    } else {
+//        this.currentPage = parseInt(this.$('.redesign-pagination selected').text());
+        log.info('currentPage: %s', self.currentPage);
+        self.currentPage = self.currentPage+1;
+        return this.baseUrl + '/?page=' + self.currentPage.toString();
+    }
+}
+
+module.exports = exports = Spider;
