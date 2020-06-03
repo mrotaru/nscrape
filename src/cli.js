0ad9c3419a20447d7b6ec2c901ba9a5574cdbb37
Mihai Rotaru <mihai.rotaru@gmx.com>
2020-06-03 11:19:55 +0100

    infinite scrolling with headless browser

--- src/cli.js
+++ src/cli.js
@@ -5,6 +5,8 @@
 const os = require("os");
 const fs = require("fs").promises;
 const axios = require("axios");
+// const puppeteer = require("puppeteer-core");
+const { webkit } = require("playwright-webkit");
 
 const { Logger, LogLevels } = require("./logger");
 const { Spider } = require("./spider");
@@ -21,13 +23,14 @@ let cli = require("commander")
   .option("--show-debug", "Show debugging information - most verbose output", false)
   .option("-v, --verbose", "Verbose output (same as --show-debug)", false)
   .option("--dump-html", "Store the last fetched HTML as a file", false)
-  .option("--reuse-html", "Reuse the previously downloaded HTML", false)
+  .option("--reuse-html", "Reuse the previously fetched HTML", false)
+  .option("--use-headless-browser", "Use a headless browser instead of axios/http", false)
   .parse(process.argv);
 
 let logLevel = LogLevels.ERROR;
-logLevel = cli.showWarnings && LogLevels.WARNING || LogLevels.ERROR;
-logLevel = cli.showInfo && LogLevels.INFO || LogLevels.WARNING;
-logLevel = cli.showDebug && LogLevels.DEBUG || LogLevels.INFO;
+(cli.showDebug || cli.verbose) && (logLevel = LogLevels.DEBUG);
+cli.showInfo && (logLevel = LogLevels.INFO);
+cli.showWarnings && (logLevel = LogLevels.WARNING);
 
 const dumpFileName = "./html-dump.html"
 
@@ -37,31 +40,83 @@ const run = async () => {
   logger.info("starting up...");
 
   const json = await fs.readFile(cli.spider, { encoding: "utf8"});
-  const spider = new Spider(JSON.parse(json));
+  const spider = new Spider(JSON.parse(json), {
+    logLevel,
+  });
+  const spiderSpec = spider.specification;
 
-  let processedPages = 0
-  while (processedPages < cli.nrOfPages) {
+  let processedPages = 0;
+
+  let browser;
+  let page;
+  const headlessConfig = spiderSpec["headless-config"];
+  let headless = spiderSpec["infinite-scrolling"] || spiderSpec["use-headless-browser"] || cli.headless;
+  if (headless) {
+    logger.info(`using a headless browser, playwright-webkit`);
+    browser = await webkit.launch();
+    page = await browser.newPage();
     const url = spider.getNextUrl();
+    logger.info(`navigating to ${url}...`);
+    await page.goto(url);
+  }
+
+  while (processedPages < cli.nrOfPages) {
     let html;
-    if (cli.reuseHtml) {
-      logger.info(`loading HTML from ${url}...`)
-      html = await fs.readFile(dumpFileName, { encoding: "utf8" });
-    } else {
-      logger.info(`downloading ${url}...`)
-      const response = await axios.get(url);
-      html = response.data;
-      if (cli.dumpHtml) {
-        await fs.writeFile(dumpFileName, html);
+    if (headless) {
+      const waitForSelector = headlessConfig?.["wait-for-element"];
+      const waitMilliseconds = headlessConfig?.["wait-milliseconds"];
+      if (waitForSelector || waitMilliseconds) {
+        const start = Date.now();
+        if (waitForSelector) {
+          logger.info(`waiting for selector ${waitForSelector}...`);
+          await page.waitForSelector(waitForSelector);
+          logger.info(`waited for selector ${Date.now() - start}ms...`);
+        }
+        if (waitMilliseconds) {
+          logger.info(`waiting ${waitMilliseconds}ms on the headless page...`);
+          await page.waitForTimeout(parseInt(waitMilliseconds));
+        }
+        html = await page.content();
+        if (cli.dumpHtml) {
+          logger.info(`dumping HTML (${html.length}) to ${dumpFileName}...`)
+          await fs.writeFile(dumpFileName, html);
+        }
       }
+      processedPages++;
+      if (processedPages >= cli.nrOfPages) {
+        await browser.close();
+      } else {
+        logger.info(`simulating pressing "End" on the keyboard...`);
+        await page.keyboard.press("End");
+      }
+    } else { // not headless - use axios to fetch HTML
+      const url = spider.getNextUrl();
+      if (cli.reuseHtml) {
+        logger.info(`loading HTML from ${url}...`)
+        html = await fs.readFile(dumpFileName, { encoding: "utf8" });
+      } else {
+        logger.info(`downloading ${url}...`)
+        const response = await axios.get(url);
+        html = response.data;
+        if (cli.dumpHtml) {
+          await fs.writeFile(dumpFileName, html);
+        }
+      }
+      processedPages++;
     }
     const items = spider.extract(html);
-    console.log(items);
-    processedPages++;
-    logger.info(`waiting ${cli.wait}ms...`)
-    await delay(cli.wait);
+    logger.info(`extracted ${items.length} items:`)
+    items.forEach(item => console.log(item));
+    if (processedPages < cli.nrOfPages) {
+      logger.info(`waiting ${cli.wait}ms...`)
+      await delay(cli.wait);
+    }
   }
+  return;
 }
 
-run().catch(err => {
+run().then(res => {
+  logger.info("done.");
+}).catch(err => {
   console.error("Error: ", err);
 });
--- src/spider.js
+++ src/spider.js
@@ -1,16 +1,22 @@
 const deepmerge = require("deepmerge");
+const crypto = require('crypto');
 const objectsFromHtml = require("objects-from-html");
+
+const { Logger, LogLevels } = require("./logger");
 
 class Spider {
   constructor(spiderSpecification, config = {}) {
     this.specification = spiderSpecification;
+    this.headless = spiderSpecification["infinite-scrolling"] || spiderSpecification["use-headless-browser"];
     this.config = deepmerge({}, config);
-    this.currentPage = 0;
+    this.iteration = 0;
     this.html = "";
+    this._lastHash = "";
+    this._logger = new Logger("spider", config.logLevel || LogLevels.ERROR);
   }
 
   getNextUrl() {
-    if (this.currentPage === 0) {
+    if (this.headless || this.iteration === 0) {
       return this.specification.baseUrl;
     } else {
       const { nextUrlDescriptor } = this.specification;
@@ -24,10 +30,32 @@ class Spider {
 
   extract(html) {
     const { itemTypes } = this.specification;
+    let returnedItems = [];
     for (let itemType of itemTypes) {
-      return objectsFromHtml(html, itemType);
+      const allItems = objectsFromHtml(html, itemType);
+      if (this.iteration > 0 && this.headless) {
+        // FIXME - very inefficient
+        for(let i = 0; i < allItems.length; i++) {
+          let item = allItems[i];
+          let hash = this._hashItem(item);
+          if (hash === this._lastHash) {
+            this._logger.info(`returning items starting with ${i} (hash: ${this._lastHash})`);
+            returnedItems = allItems.slice(i);
+            break;
+          }
+        }
+      } else {
+        returnedItems = allItems;
+      }
     }
     this.html = html;
+    this._lastHash = this._hashItem(returnedItems[returnedItems.length -1]);
+    this.iteration++;
+    return returnedItems;
+  }
+
+  _hashItem(item) {
+    return crypto.createHash('md5').update(JSON.stringify(item)).digest('hex');
   }
 }
 
